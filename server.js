e6e79785f542199adb22004b1cf6d16588b27e05
Boba Fett <thomascupchurch@gmail.com>
2021-10-08 10:34:27 -0500

    adds server.js file

--- server.js
+++ server.js
@@ -1,16 +1,7 @@
-const router = require("express").Router();
-// Import all of the API routes from /api/index.js (no need for index.js though since it's implied)
-const apiRoutes = require("./api");
-const htmlRoutes = require("./html/html-routes");
+const mongoose = require("mongoose");
+const express = require("express");
 
-// add prefix of `/api` to all if the api routes imported from the 'api' directory
-router.use("/api", apiRoutes);
-router.use("/", htmlRoutes);
-
-router.use((req, res) => {
-  res.status(404).send("<h1>😝 404 Error!</h1>");
-});
-
+const app = express();
 const PORT = process.env.PORT || 3001;
 
 app.use(express.json());
@@ -28,5 +19,3 @@ mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/pizza-hunt", {
 // Use this to log mongo queries being executed
 mongoose.set("debug", true);
 app.listen(PORT, () => console.log(`🌍 Connected on localhost:${PORT}`));
-
-module.exports = router;
